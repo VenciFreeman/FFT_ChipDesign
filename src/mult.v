53b1096b10557bd0b791ad6145d5ce282af5a1f6
Venci Freeman <vencifreeman16@sjtu.edu.cn>
2020-03-30 18:48:56 +0800

    Version 1.0

---
+++ src/mult.v
@@ -0,0 +1,30 @@
+//**********************************************************
+// Author: @VenciFreeman, copyright 2020
+// e-mail: vencifreeman16@sjtu.edu.cn
+// School: Shanghai Jiao Tong University
+//
+// File Name: butterfly
+//
+// Type: Combinational
+//
+// Purpose: <Specific Function Description>
+// 
+//
+// Details:
+//
+//
+// Release History:
+// - Version 1.0 20/03/30: Create;
+//
+// Notes:
+//
+//**********************************************************
+
+module mult(a_in, b_in, c_out)
+
+  input a_in[16:0];
+  input b_in[7:0];
+
+  output c_out[16:0];
+
+endmodule
