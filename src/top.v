83436c275caf4d3bca541d3755a6ad780d3a54b7
Venci Freeman <vencifreeman16@sjtu.edu.cn>
2020-03-30 18:49:05 +0800

    Update top.v

--- src/top.v
+++ src/top.v
@@ -35,54 +35,54 @@ module top(clk, rst_n, data_in, data out)
 
   input clk;  // clock
   input rst_n;  // reset signal
-	input data_in[33:0];  // <Specific Description> 
+  input data_in[33:0];  // <Specific Description> 
 
   output data_out[33:0];  // <Specific Description>  
 
   wire clk;
   wire rst_n;
-	wire [33:0] data_in;
+  wire [33:0] data_in;
 
   reg [33:0] data_out;  // All the output signals should use reg
 
-	ctrl ctrl0(
-		.clk(clk),
-		.rst_n(rst_n),
-		.s_p_flag_out(s_p_flag_out),
-		.s_p_lag_mux(s_p_flag_mux),
-		.reg1_flag_mux(reg1_flag_mux),
-		.mux_flag(mux_flag),
-		.rotation(rotation),
-		.demux_flag(demux_flag)
-	);
+  ctrl ctrl0(
+    .clk(clk),
+    .rst_n(rst_n),
+    .s_p_flag_out(s_p_flag_out),
+	.s_p_lag_mux(s_p_flag_mux),
+    .reg1_flag_mux(reg1_flag_mux),
+    .mux_flag(mux_flag),
+    .rotation(rotation),
+	.demux_flag(demux_flag)
+);
 
-	s_p s_p0(
-		.clk(clk),
-		.rst_n(rst_n),
-		.data_in_0(data_in),
-		.data_out_0(data_out_0)
-	);
+  s_p s_p0(
+	.clk(clk),
+	.rst_n(rst_n),
+	.data_in_0(data_in),
+	.data_out_0(data_out_0)
+  );
 
-	mux mux0(
-		data_out_0(data_out_0),
-		data_out_1(data_out_1),
-		clac_in(clac_in)
-	);
+  mux mux0(
+	data_out_0(data_out_0),
+	data_out_1(data_out_1),
+	clac_in(clac_in)
+  );
 
-	butterfly butterfly0(
+  butterfly butterfly0(
 
-	);
+  );
 
-	reg2 reg20(
+  reg2 reg20(
 
-	);
+  );
 
-	demux demux0(
+  demux demux0(
 
-	);
+  );
 
-	p_s p_s0(
+  p_s p_s0(
 
-	);
+  );
 
 endmodule
