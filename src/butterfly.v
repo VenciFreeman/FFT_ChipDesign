8c74d33e389c28bea6d65f089cc69132e3c23cb5
Venci Freeman <vencifreeman16@sjtu.edu.cn>
2020-04-06 16:49:02 +0800

    Fix some errors.

--- src/butterfly.v
+++ src/butterfly.v
@@ -7,29 +7,29 @@
 //
 // Type: Combinational
 //
-// Purpose: <Specific Function Description>
-// 
+// Purpose: Butterfly computation for 16-bit 4-radix FFT.
 //
-// Details:
+// Details: 
 //
 //
 // Release History:
 // - Version 1.0 20/03/27: Create;
-// - Version 1.1 20/02/20: Modify.
+// - Version 1.1 20/03/20: Modify;
+// - Version 1.2 20/04/06. Modify the multiply method.
 //
 // Notes:
 //
 //**********************************************************
 
-module butterfly(clac_in, rotation_factors, calc_out)
+module butterfly(
 
-  input clac_in[135:0];  // <Specific Description> 
-  input rotation_factors[23:0];  // Each
+  input  wire[135:0] clac_in,  // The 4 numbers which need to be calculated. Format: in4(Re,Im), in3(Re,Im), in2(Re,Im), in1(Re,Im)
+  input  wire[23:0]  rotation_factors,  // rotation factors of each butterfly computation
+  output reg[135:0]  calc_out  // The 4 output numbers. Format: out4(Re,Im), out3(Re,Im), out2(Re,Im), out1(Re,Im)
 
-  output calc_out[135:0];  // <Specific Description>  
+  );
 
-  wire [135:0] clac_in;
-  wire signed [16:0] row1_1_real, row1_1_imag;
+  wire signed [16:0] row1_1_real, row1_1_imag;  // row means stage of FFT. 16-point 4-radix FFT has 2 stages.
   wire signed [16:0] row1_2_real, row1_2_imag;
   wire signed [16:0] row1_3_real, row1_3_imag;
   wire signed [16:0] row1_4_real, row1_4_imag;
@@ -38,16 +38,14 @@ module butterfly(clac_in, rotation_factors, calc_out)
   wire signed [16:0] row2_3_real, row2_3_imag;
   wire signed [16:0] row2_4_real, row2_4_imag;
 
-  reg [135:0] clac_out;  // All the output signals should use reg
-
   assign row1_1_real = clac_in[33:17];
   assign row1_1_imag = clac_in[16:0];
-  assign row1_2_real = clac_in[67:51] * rotation_factors[7:0];
-  assign row1_2_imag = clac_in[50:34] * rotation_factors[7:0];
-  assign row1_3_real = clac_in[101:85] * rotation_factors[15:8];
-  assign row1_3_imag = clac_in[84:68] * rotation_factors[15:8];
-  assign row1_4_real = clac_in[135:119] * rotation_factors[23:16];
-  assign row1_4_imag = clac_in[118:102] * rotation_factors[23:16];
+  assign row1_2_real = clac_in[67:51] >> rotation_factors[7:0];  // Need the specific figures to rewrite as shift operation.
+  assign row1_2_imag = clac_in[50:34] >> rotation_factors[7:0];
+  assign row1_3_real = clac_in[101:85] >> rotation_factors[15:8];
+  assign row1_3_imag = clac_in[84:68] >> rotation_factors[15:8];
+  assign row1_4_real = clac_in[135:119] >> rotation_factors[23:16];
+  assign row1_4_imag = clac_in[118:102] >> rotation_factors[23:16];
 
   assign row2_1_real = row1_1_real + row1_3_real;
   assign row2_1_imag = row1_1_imag + row1_3_imag;
@@ -58,9 +56,9 @@ module butterfly(clac_in, rotation_factors, calc_out)
   assign row2_4_real = row1_2_real - row1_4_real;
   assign row2_4_imag = row1_2_imag - row1_4_imag;
 
-  assign clac_out = { (row2_2_real - row2_4_real) : (row2_2_imag - row2_4_imag)
-                    : (row2_2_real + row2_4_real) : (row2_2_imag + row2_4_imag)
-                    : (row2_1_real - row2_3_real) : (row2_1_imag - row2_3_imag)
-                    : (row2_1_real + row2_3_real) : (row2_1_imag + row2_3_imag)};
+  assign clac_out = { (row2_2_real - row2_4_real) , (row2_2_imag - row2_4_imag)
+                    , (row2_2_real + row2_4_real) , (row2_2_imag + row2_4_imag)
+                    , (row2_1_real - row2_3_real) , (row2_1_imag - row2_3_imag)
+                    , (row2_1_real + row2_3_real) , (row2_1_imag + row2_3_imag)};
 
 endmodule
