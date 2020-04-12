db1951164b008f5a6c096cd9e8b127c1c0577193
Venci Freeman <vencifreeman16@sjtu.edu.cn>
2020-04-12 10:13:12 +0800

    Version 3.0 20/04/12: Transform to a decimal multiplier.

--- src/multi16.v
+++ src/multi16.v
@@ -13,7 +13,8 @@
 //
 // Release History:
 // - Version 1.0 20/04/09: Create;
-// - Version 2.0 20/04/09: Modify.
+// - Version 2.0 20/04/09: Modify;
+// - Version 3.0 20/04/12: Transform to a decimal multiplier.
 //
 // Notes:
 //
@@ -21,42 +22,45 @@
 
 module multi16(
 
-  input [15:0] a_in,
-  input [15:0] b_in,
-  output[15:0] result,
-
+  input clk,
+  input rst_n,
+  input [15:0] in_16bit,
+  input [7:0] in_8bit,
+  output reg[15:0] out
+  
   );
 
-  reg[15:0] areg;
-  reg[15:0] breg;
-  reg[15:0] result_r;
-  reg done_r;
-  reg[4:0] i;  // count
+  reg flag;
+  reg [15:0] in_16bit_b;
+  reg [7:0]  in_8bit_b;
+  reg [21:0] sum;
+  reg [23:0] sum_b;
 
-  assign done = done_r;
-  assign result = result_r;
+  always @(posedge clk) begin
+    if(!rst_n) begin
+      out <= 24'd0;
+      flag <= 1'b0;
+      in_16bit_b <= 16'd0;
+      in_8bit_b <= 8'd0;
+      sum <= 22'd0;
+      sum_b <= 24'd0;
+    end
+    else begin
+    // 2's Complement ==> True Form
+    in_16bit_b <= (xin_16[15] == 1) ? {xin_16[15] , ~xin_16[14:0] + 1'b1} : xin_16;
+    in_8bit_b  <= (xin_8[7]   == 1) ? {xin_8 [7]  , ~xin_8 [6:0]  + 1'b1} : xin_8;
 
-  always @ (a_in or b_in) begin
-    if (i < 5'd17)
-      i <= i + 1'b1; 
-    else
-      i <= 5'd0;
-  end
+    // Determine the sign of the product
+    flag <= in_16bit_b[15] ^ in_8bit_b[7];
 
-  always @ (a_in or b_in) begin
-    if (i == 5'd0)
-      areg <= a_in;
-  end
-
-  always @ (a_in or b_in) begin
-    if (i == 5'd0)
-      breg <= b_in;
-  end
-
-  always @ (a_in or b_in) begin
-    if (i > 5'd0 && i < 5'd17)
-      if ( areg[i - 1] )
-        result_r = result_r + (breg << (i - 1));
+    // Calculate decimal
+    sum <= in_16bit_b[14:0] * in_8bit_b[6:0];
+    
+    // The signed product represented by true form
+    sum_b <= {flag, sum, 1'b0};
+    
+    // True Form ==> 2's Complement
+    out <= sum_b[23] ? {sum_b[23], ~sum_b[22:9] + 1'b1} : sum_b[23:8];
   end
 
 endmodule
