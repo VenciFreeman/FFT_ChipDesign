b6d07f4dcf5a05f691ba4794bca47d8c9cded586
Venci Freeman <vencifreeman16@sjtu.edu.cn>
2020-04-12 17:58:35 +0800

    Version 3.3 20/04/12: Fix some errors, compile successful.

--- src/multi16.v
+++ src/multi16.v
@@ -16,7 +16,8 @@
 // - Version 2.0 20/04/09: Modify;
 // - Version 3.0 20/04/12: Transform to a decimal multiplier;
 // - Version 3.1 20/04/12: Rewrite always parts;
-// - Version 3.2 20/04/12: Add rst_n to the sensitive list.
+// - Version 3.2 20/04/12: Add rst_n to the sensitive list;
+// - Version 3.3 20/04/12: Fix some errors, compile successful.
 //
 // Notes:
 //
@@ -38,42 +39,42 @@ module multi16(
   reg [21:0] sum;
   reg [23:0] sum_b;
 
-  always @(posedge clk or negedge ret_n) begin
+  always @(posedge clk or negedge rst_n) begin
     if( !rst_n )
       in_16bit_b <= 16'b0;
     else
-      in_16bit_b <= (xin_16[15] == 1) ? {xin_16[15] , ~xin_16[14:0] + 1'b1} : xin_16;  // 2's Complement ==> True Form
+      in_16bit_b <= (in_16bit[15] == 1) ? {in_16bit[15] , ~in_16bit[14:0] + 1'b1} : in_16bit;  // 2's Complement ==> True Form
   end
 
-  always @(posedge clk or negedge ret_n) begin
+  always @(posedge clk or negedge rst_n) begin
     if( !rst_n )
       in_8bit_b <= 8'b0;
     else
-      in_8bit_b  <= (xin_8[7]   == 1) ? {xin_8 [7]  , ~xin_8 [6:0]  + 1'b1} : xin_8;  // 2's Complement ==> True Form
+      in_8bit_b  <= (in_8bit[7]   == 1) ? {in_8bit[7]  , ~in_8bit[6:0]  + 1'b1} : in_8bit;  // 2's Complement ==> True Form
   end
 
-  always @(posedge clk or negedge ret_n) begin
+  always @(posedge clk or negedge rst_n) begin
     if( !rst_n )
       flag <= 1'b0;
     else
       flag <= in_16bit_b[15] ^ in_8bit_b[7];  // Determine the sign of the product
   end
 
-  always @(posedge clk or negedge ret_n) begin
+  always @(posedge clk or negedge rst_n) begin
     if( !rst_n )
       sum <= 22'b0;
     else
       sum <= in_16bit_b[14:0] * in_8bit_b[6:0];  // Calculate decimal
   end
 
-  always @(posedge clk or negedge ret_n) begin
+  always @(posedge clk or negedge rst_n) begin
     if( !rst_n )
       sum_b <= 24'b0;
     else
       sum_b <= {flag, sum, 1'b0};  // The signed product represented by true form
   end
 
-  always @(posedge clk or negedge ret_n) begin
+  always @(posedge clk or negedge rst_n) begin
     if( !rst_n )
       out <= 24'b0;
     else
