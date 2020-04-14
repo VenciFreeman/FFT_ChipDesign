718a0ca8c8c60a5f1f794e4ff3f331d4308872e6
Venci Freeman <vencifreeman16@sjtu.edu.cn>
2020-04-14 23:13:33 +0800

    Version 2.2 20/04/14: Update rotation factors.

--- src/butterfly.v
+++ src/butterfly.v
@@ -25,7 +25,8 @@
 // - Version 1.6 20/04/12: Use non-blocking assignment, transform to sequential;
 // - Version 1.7 20/04/12: Edit decimal multiplier, rewrite wire/reg;
 // - Version 2.0 20/04/12: Fix some errors, compile successful;
-// - Version 2.1 20/04/12: Optimize layout, change to 2's complement and add notes.
+// - Version 2.1 20/04/12: Optimize layout, change to 2's complement and add notes;
+// - Version 2.2 20/04/14: Update rotation factors.
 //
 // Notes:
 // - rotation_factor format: (Re,Im);
@@ -47,13 +48,12 @@ module butterfly(
 
   parameter para0000 = 8'b00000000;  //  0.0000. sin(0pi/8) = sin(8pi/8) = cos(4pi/8) = - cos(4pi/8)
   parameter para3827 = 8'b01100001;  //  0.3827. sin(1pi/8) = sin(7pi/8) = cos(3pi/8) = - cos(5pi/8)
-  parameter parn3827 = 8'b11100001;  // -0.3827. sin(1pi/8) = sin(7pi/8) = cos(3pi/8) = - cos(5pi/8)
+  parameter parn3827 = 8'b10011111;  // -0.3827. sin(1pi/8) = sin(7pi/8) = cos(3pi/8) = - cos(5pi/8)
   parameter para7071 = 8'b01011010;  //  0.7071. sin(2pi/8) = sin(6pi/8) = cos(2pi/8) = - cos(6pi/8)
-  parameter parn7071 = 8'b11011010;  // -0.7071. sin(2pi/8) = sin(6pi/8) = cos(2pi/8) = - cos(6pi/8)
+  parameter parn7071 = 8'b10100110;  // -0.7071. sin(2pi/8) = sin(6pi/8) = cos(2pi/8) = - cos(6pi/8)
   parameter para9239 = 8'b01110110;  //  0.9239. sin(3pi/8) = sin(5pi/8) = cos(1pi/8) = - cos(7pi/8)
-  parameter parn9239 = 8'b11110110;  // -0.9239. sin(3pi/8) = sin(5pi/8) = cos(1pi/8) = - cos(7pi/8)
+  parameter parn9239 = 8'b10001010;  // -0.9239. sin(3pi/8) = sin(5pi/8) = cos(1pi/8) = - cos(7pi/8)
   parameter para1111 = 8'b01111111;  //  1.0000. sin(4pi/8) = sin(4pi/8) = coa(0pi/8) = - cos(8pi/8)
-  parameter parn1111 = 8'b11111111;  // -1.0000. sin(4pi/8) = sin(4pi/8) = coa(0pi/8) = - cos(8pi/8)
   
   reg  [15:0] rotation_factor1;  // For input B
   reg  [15:0] rotation_factor2;  // For input C
@@ -86,35 +86,35 @@ module butterfly(
   always @ ( posedge clk ) begin
     case ( rotation )
       3'b000: begin
-        rotation_factor1[15:8] <= para1111;  // cos(0) = 1
-        rotation_factor1[7:0]  <= para0000;  // sin(0) = 0
+        rotation_factor1[15:8] <= para1111;
+        rotation_factor1[7:0]  <= para0000;
       end
       3'b001: begin
-        rotation_factor1[15:8] <= para0000;  // cos(pi/2) = 0
-        rotation_factor1[7:0]  <= para1111;  // sin(pi/2) = 1
+        rotation_factor1[15:8] <= para1111;
+        rotation_factor1[7:0]  <= para0000;
       end
       3'b010: begin
-        rotation_factor1[15:8] <= parn1111;  // cos(pi) = -1
-        rotation_factor1[7:0]  <= para0000;  // sin(pi) = 0
+        rotation_factor1[15:8] <= para1111;
+        rotation_factor1[7:0]  <= para0000;
       end
       3'b011: begin
-        rotation_factor1[15:8] <= para0000;  // cos(3pi/2) = 0
-        rotation_factor1[7:0]  <= parn1111;  // sin(3pi/2) = -1
+        rotation_factor1[15:8] <= para1111;
+        rotation_factor1[7:0]  <= para0000;
       end
       3'b100: begin
-        rotation_factor1[15:8] <= para1111;  // cos(0) = 1
+        rotation_factor1[15:8] <= para1111;  // cos(0) = 1            W_16^0
         rotation_factor1[7:0]  <= para0000;  // sin(0) = 0
       end
       3'b101: begin
-        rotation_factor1[15:8] <= para9239;  // cos(pi/8) = 0.9239
+        rotation_factor1[15:8] <= para9239;  // cos(pi/8) = 0.9239    W_16^1
         rotation_factor1[7:0]  <= para3827;  // sin(pi/8) = 0.3827
       end
       3'b110: begin
-        rotation_factor1[15:8] <= para7071;  // cos(pi/4) = 0.7071
+        rotation_factor1[15:8] <= para7071;  // cos(pi/4) = 0.7071    W_16^2
         rotation_factor1[7:0]  <= para7071;  // sin(pi/4) = 0.7071
       end
       3'b111: begin
-        rotation_factor1[15:8] <= para3827;  // cos(3pi/8) = 0.3827
+        rotation_factor1[15:8] <= para3827;  // cos(3pi/8) = 0.3827   W_16^3
         rotation_factor1[7:0]  <= para9239;  // sin(3pi/8) = 0.9239
       end
     endcase
@@ -124,35 +124,35 @@ module butterfly(
   always @ ( posedge clk ) begin
     case ( rotation )
       3'b000: begin
-        rotation_factor2[15:8] <= para1111;  // cos(0) = 1
-        rotation_factor2[7:0]  <= para0000;  // sin(0) = 0
+        rotation_factor2[15:8] <= para1111;
+        rotation_factor2[7:0]  <= para0000;
       end
       3'b001: begin
-        rotation_factor2[15:8] <= parn1111;  // cos(pi) = -1
-        rotation_factor2[7:0]  <= para0000;  // sin(pi) = 0
+        rotation_factor2[15:8] <= para1111;
+        rotation_factor2[7:0]  <= para0000;
       end
       3'b010: begin
-        rotation_factor2[15:8] <= para1111;  // cos(2pi) = 1
-        rotation_factor2[7:0]  <= para0000;  // sin(2pi) = 0
+        rotation_factor2[15:8] <= para1111;
+        rotation_factor2[7:0]  <= para0000;
       end
       3'b011: begin
-        rotation_factor2[15:8] <= parn1111;  // cos(3pi) = -1
-        rotation_factor2[7:0]  <= para0000;  // sin(3pi) = 0
+        rotation_factor2[15:8] <= para1111;
+        rotation_factor2[7:0]  <= para0000;
       end
       3'b100: begin
-        rotation_factor2[15:8] <= para1111;  // cos(0) = 1
+        rotation_factor2[15:8] <= para1111;  // cos(0) = 1            W_16^0
         rotation_factor2[7:0]  <= para0000;  // sin(0) = 0
       end
       3'b101: begin
-        rotation_factor2[15:8] <= para7071;  // cos(pi/4) = 0.7071
+        rotation_factor2[15:8] <= para7071;  // cos(pi/4) = 0.7071    W_16^2
         rotation_factor2[7:0]  <= para7071;  // sin(pi/4) = 0.7071
       end
       3'b110: begin
-        rotation_factor2[15:8] <= para0000;  // cos(pi/2) = 0
+        rotation_factor2[15:8] <= para0000;  // cos(pi/2) = 0         W_16^4
         rotation_factor2[7:0]  <= para1111;  // sin(pi/2) = 1
       end
       3'b111: begin
-        rotation_factor2[15:8] <= parn7071;  // cos(3pi/4) = -0.7071
+        rotation_factor2[15:8] <= parn7071;  // cos(3pi/4) = -0.7071  W_16^6
         rotation_factor2[7:0]  <= para7071;  // sin(3pi/4) = 0.7071
       end
     endcase
@@ -162,35 +162,35 @@ module butterfly(
   always @ ( posedge clk ) begin
     case ( rotation )
       3'b000: begin
-        rotation_factor3[16:8] <= para1111;  // cos(0) = 1
-        rotation_factor3[7:0]  <= para0000;  // sin(0) = 0
+        rotation_factor3[16:8] <= para1111;
+        rotation_factor3[7:0]  <= para0000;
       end
       3'b001: begin
-        rotation_factor3[16:8] <= para0000;  // cos(3pi/2) = 0
-        rotation_factor3[7:0]  <= parn1111;  // sin(3pi/2) = -1
+        rotation_factor3[16:8] <= para1111;
+        rotation_factor3[7:0]  <= para0000;
       end
       3'b010: begin
-        rotation_factor3[16:8] <= parn1111;  // cos(3pi) = -1
-        rotation_factor3[7:0]  <= para0000;  // sin(3pi) = 0
+        rotation_factor3[16:8] <= para1111;
+        rotation_factor3[7:0]  <= para0000;
       end
       3'b011: begin
-        rotation_factor3[16:8] <= para0000;  // cos(9pi/2) = 0
-        rotation_factor3[7:0]  <= para1111;  // sin(9pi/2) = 1
+        rotation_factor3[16:8] <= para1111;
+        rotation_factor3[7:0]  <= para0000;
       end
       3'b100: begin
-        rotation_factor3[16:8] <= para1111;  // cos(0) = 1
+        rotation_factor3[16:8] <= para1111;  // cos(0) = 1            W_16^0
         rotation_factor3[7:0]  <= para0000;  // sin(0) = 0
       end
       3'b101: begin
-        rotation_factor3[16:8] <= para3827;  // cos(3pi/4) = 0.3827
+        rotation_factor3[16:8] <= para3827;  // cos(3pi/4) = 0.3827   W_16^3
         rotation_factor3[7:0]  <= para9239;  // sin(3pi/4) = 0.9239
       end
       3'b110: begin
-        rotation_factor3[16:8] <= parn7071;  // cos(3pi/4) = -0.7071
+        rotation_factor3[16:8] <= parn7071;  // cos(3pi/4) = -0.7071  W_16^6
         rotation_factor3[7:0]  <= para7071;  // sin(3pi/4) = 0.7071
       end
       3'b111: begin
-        rotation_factor3[16:8] <= parn9239;  // cos(9pi/8) = -0.9239
+        rotation_factor3[16:8] <= parn9239;  // cos(9pi/8) = -0.9239  W_16^9
         rotation_factor3[7:0]  <= parn3827;  // sin(9pi/8) = -0.3827
       end
     endcase
@@ -214,63 +214,63 @@ module butterfly(
 
   multi16 multi1_2_1 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_1_1),
+                      .in_17bit(in_16bit_1_1),
                       .in_8bit(in_8bit_1_1),
                       .out(comp_part_1)
                      );
 
   multi16 multi1_2_2 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_1_2),
+                      .in_17bit(in_16bit_1_2),
                       .in_8bit(in_8bit_1_2),
                       .out(row1_2_real_b)
                       );
 
   multi16 multi1_2_3 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_1_3),
+                      .in_17bit(in_16bit_1_3),
                       .in_8bit(in_8bit_1_3),
                       .out(row1_2_imag_b)
                       );
 
   multi16 multi1_3_1 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_2_1),
+                      .in_17bit(in_16bit_2_1),
                       .in_8bit(in_8bit_2_1),
                       .out(comp_part_2)
                      );
 
   multi16 multi1_3_2 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_2_2),
+                      .in_17bit(in_16bit_2_2),
                       .in_8bit(in_8bit_2_2),
                       .out(row1_3_real_b)
                       );
 
   multi16 multi1_3_3 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_1_3),
+                      .in_17bit(in_16bit_1_3),
                       .in_8bit(in_8bit_1_3),
                       .out(row1_3_imag_b)
                       );
 
   multi16 multi1_4_1 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_3_1),
+                      .in_17bit(in_16bit_3_1),
                       .in_8bit(in_8bit_3_1),
                       .out(comp_part_3)
                      );
 
   multi16 multi1_4_2 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_3_2),
+                      .in_17bit(in_16bit_3_2),
                       .in_8bit(in_8bit_3_2),
                       .out(row1_4_real_b)
                       );
 
   multi16 multi1_4_3 (.clk(clk),
                       .rst_n(rst_n),
-                      .in_16bit(in_16bit_3_3),
+                      .in_17bit(in_16bit_3_3),
                       .in_8bit(in_8bit_3_3),
                       .out(row1_4_imag_b)
                       );
