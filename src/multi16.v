fa79f9d73f3000b96f2d55a9aef9331670099669
Venci Freeman <vencifreeman16@sjtu.edu.cn>
2020-04-17 08:20:55 +0800

    Version 3.8 20/04/17: Check again, fix some errors and add comments.

--- src/multi16.v
+++ src/multi16.v
@@ -21,7 +21,8 @@
 // - Version 3.4 20/04/12: Add comments;
 // - Version 3.5 20/04/14: Transform to 17-bit input;
 // - Version 3.6 20/04/16: Fix some simulation errors;
-// - Version 3.7 20/04/16: Simulate successful. Still need to check results.
+// - Version 3.7 20/04/16: Simulate successful. Still need to check results;
+// - Version 3.8 20/04/17: Check again, fix some errors and add comments.
 //
 // Notes: 
 //
@@ -29,8 +30,8 @@
 
 module multi16(
 
-  input wire clk,              // clock
-  input wire rst_n,            // reset
+  input wire        clk,       // clock
+  input wire        rst_n,     // reset
   input wire [16:0] in_17bit,  // 17-bit input  data
   input wire [7:0]  in_8bit,   // 8-bit  input  data
   output reg [16:0] out        // 17-bit output data
@@ -44,51 +45,56 @@ module multi16(
   reg [23:0] sum_b;
 
 // This always part controls signal in_17bit_b. Feature: 2's Complement ==> True Form
-  always @(posedge clk or negedge rst_n) begin
+  always @ (posedge clk or negedge rst_n) begin
     if( !rst_n )
       in_17bit_b <= 17'b0;
     else
       in_17bit_b <= (in_17bit[16] == 1) ? {in_17bit[16] , ~in_17bit[14:0] + 1'b1} : in_17bit;
+    // If in_17bit is a negative number, transform to 2's complement, otherwise remain the same.
   end
 
 // This always part controls signal in_8bit_b. Feature: 2's Complement ==> True Form
-  always @(posedge clk or negedge rst_n) begin
+  always @ (posedge clk or negedge rst_n) begin
     if( !rst_n )
       in_8bit_b <= 8'b0;
     else
       in_8bit_b <= (in_8bit[7] == 1) ? {in_8bit[7], ~in_8bit[6:0] + 1'b1} : in_8bit;
+    // If in_8bit is a negative number, transform to 2's complement, otherwise remain the same.
   end
 
 // This always part controls signal flag. Feature: Determine the sign of the product
-  always @(posedge clk or negedge rst_n) begin
+  always @ ( posedge clk or negedge rst_n ) begin
     if( !rst_n )
       flag <= 1'b0;
     else
       flag <= in_17bit_b[16] ^ in_8bit_b[7];
+    // Determine the sign of the product.      
   end
 
 // This always part controls signal sum. Feature: Calculate decimal
-  always @(posedge clk or negedge rst_n) begin
+  always @ ( posedge clk or negedge rst_n ) begin
     if( !rst_n )
       sum <= 22'b0;
     else
-      sum <= in_17bit_b[14:0] * in_8bit_b[6:0];
+      sum <= in_17bit_b[15:0] * in_8bit_b[6:0];
+      // Calculate the absolute value of the product.
   end
 
 // This always part controls signal sum_b. Feature: The signed product represented by true form
-  always @(posedge clk or negedge rst_n) begin
+  always @ ( posedge clk or negedge rst_n ) begin
     if( !rst_n )
       sum_b <= 24'b0;
     else
       sum_b <= {flag, sum, 1'b0};
+    // Add the sign.
   end
 
 // This always part controls signal out. Feature: True Form ==> 2's Complement
-  always @(posedge clk or negedge rst_n) begin
+  always @ ( posedge clk or negedge rst_n ) begin
     if( !rst_n )
       out <= 17'b0;
     else
-      out <= sum_b[23] ? {sum_b[23], ~sum_b[22:9] + 1'b1} : sum_b[23:8];  // Output only keep 17 bits
+      out <= sum_b[23] ? {sum_b[23], ~sum_b[22:7] + 1'b1} : sum_b[23:7];  // Output only keep 17 bits
   end
 
 endmodule
