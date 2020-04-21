1b3b2dbf2c06752ea1c6ce8e354c41a4f1badb5f
Venci Freeman <vencifreeman16@sjtu.edu.cn>
2020-04-21 18:06:50 +0800

    Version 1.3 20/04/21: Update display method.

--- src/testbench.v
+++ src/testbench.v
@@ -17,7 +17,8 @@
 // Release History:
 // - Version 1.0 20/03/26: Create.
 // - Version 1.1 20/03/28: Create Data_Input txt file & stimuli logic.
-// - Version 1.2 20/03/29: Create easy version top to verify & debug.
+// - Version 1.2 20/03/29: Create easy version top to verify & debug;
+// - Version 1.3 20/04/21: Update display method.
 //
 // Notes:
 // - <Problems>;
@@ -45,13 +46,13 @@ end
 initial begin
 	rst_n = 1'b0;
   $display("\n\nLoad Data\n");  
-  $readmemb("test/Data_Input.txt", InputData);
-  #250 begin
+  $readmemb("../FFT_ChipDesign/src/testfile/Data_Input.txt", InputData);
+  #150 begin
     rst_n = 1'b1;
     for(i = 0; i < 16; i = i + 1) begin
-      #100 data_in = InputData[i];
-      $display("in%d:( %d,%d j)", i, data_in[33:17], data_in[16:0]);
-      $display("out%d:( %d %d j)", i, data_out[33:17], data_out[16:0]);
+      data_in = InputData[i];
+      #100
+      $display("o[%d] %b_%b_%b %b_%b_%b\n", i, data_out[33],  data_out[32:25],   data_out[24:17],   data_out[16],  data_out[15:8],    data_out[7:0]);
     end 
   end
   #100000   $stop;   
